6e7ecdc40ce7a2ef1fbd3e5cf81169c37fa822a6
Kuzivakwashe MUVEZWA <kmuvezwa@e5r1p1.wethinkcode.co.za>
2017-08-29 12:08:11 +0200

    updated load_desktop.sh

--- load_desktop.sh
+++ load_desktop.sh
@@ -6,11 +6,11 @@
 #    By: kmuvezwa <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/08/23 12:34:04 by kmuvezwa          #+#    #+#              #
-#    Updated: 2017/08/23 13:02:21 by kmuvezwa         ###   ########.fr        #
+#    Updated: 2017/08/29 12:05:30 by kmuvezwa         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 cd 
 cd ./Desktop/ && mkdir 2017
-cd ./2017/ && git clone https://github.com/TheGodOfAwesome/useful_scripts.git && git clone https://github.com/TheGodOfAwesome/libft.git
+cd ./2017/ && git clone https://github.com/TheGodOfAwesome/useful_scripts.git && git clone https://github.com/TheGodOfAwesome/libft.git && git clone https://github.com/TheGodOfAwesome/ft_ls.git
